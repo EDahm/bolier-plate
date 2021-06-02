123662d754d4e7c089696bc327ad068c456ccda5
EDahm <2dahm@daum.net>
2021-06-02 21:26:50 +0900

    리액트 기초 #21 데이터 Flow & Axios

--- server/index.js
+++ server/index.js
@@ -29,6 +29,12 @@ app.get('/', (req, res) => {
 })
 
 
+
+app.get('/api/hello', (req, res) => {
+    res.send("안녕하세요")
+})
+
+
 app.post('/api/users/register', (req, res) => {
 
     //회원가입 할때 필욯안 정보들을 client에서 가져오면
